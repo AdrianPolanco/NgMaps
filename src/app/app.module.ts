00fc47a11869fe1262c16bb45cc1ea846b913ea5
Adrian <adferrer0611@gmail.com>
2024-08-20 10:02:58 -0400

    HashLocation strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { APP_BASE_HREF } from '@angular/common';
+import { APP_BASE_HREF, HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [{ provide: APP_BASE_HREF, useValue: '/NgMaps/' }],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/NgMaps/' },
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
